629107bf18791d849e9feaa79289fdd71c35abda
Derick Eugenio <eugenioderick@gmail.com>
2022-05-19 23:49:35 +0800

    added bg on home

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,5 +1,53 @@
-<div class="page-container order-payment-container">
+<div class="page-container home-container">
 	<div class="container-body" style="top: 0px;">
-        
+		<div class="carousel-container">
+	        <div id="carousel-example-2" class="carousel slide carousel-fade z-depth-1-half" data-ride="carousel">
+	        	<ol class="carousel-indicators">
+			        <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+			        <li data-target="#carousel-example-2" data-slide-to="1"></li>
+			        <li data-target="#carousel-example-2" data-slide-to="2"></li>
+			    </ol>
+
+			    <div class="carousel-inner" role="listbox">
+			    	<div class="carousel-item active">
+						<div class="view">
+							<img class="d-block" src="<?= base_url();?>assets/uploads/images/adu-slide1.jpg" alt="First slide">
+							<div class="mask rgba-black-light"></div>
+						</div>
+						<div class="carousel-caption">
+							<h3 class="h3-responsive">AdU</h3>
+							<!-- <p>AdU Caption</p> -->
+						</div>
+			        </div>
+			        <div class="carousel-item">
+						<div class="view">
+							<img class="d-block" src="<?= base_url();?>assets/uploads/images/adu-slide2.jpg" alt="Second slide">
+							<div class="mask rgba-black-strong"></div>
+						</div>
+						<div class="carousel-caption">
+							<h3 class="h3-responsive">AdU 2</h3>
+						</div>
+			        </div>
+			        <div class="carousel-item">
+						<div class="view">
+							<img class="d-block" src="<?= base_url();?>assets/uploads/images/adu-slide3.jpg" alt="Third slide">
+							<div class="mask rgba-black-slight"></div>
+							</div>
+						<div class="carousel-caption">
+							<h3 class="h3-responsive">AdU 3</h3>
+						</div>
+			        </div>
+				</div>
+
+				<a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
     </div>
 </div>
