b667756754b277291485a9c1b41ab2da12145e59
Derick Eugenio <eugenioderick@gmail.com>
2022-09-08 10:36:25 +0800

    CHANGES

--- application/views/user/customer.php
+++ application/views/user/customer.php
@@ -17,6 +17,7 @@
 	                        <th class="th-name">Name</th>
 	                        <th class="th-email">Email</th>
 	                        <th class="th-verified">Verified?</th>
+	                        <th class="th-status">Status</th>
 	                        <th class="th-action">Action</th>
 	                    </tr>
 	                </thead>
@@ -27,6 +28,52 @@
 			<div class="process-loading-container"></div>
 		</div>
 	</div>
+
+	<div class="modal fade" id="change_status_modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Change Status</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                	<div class="form-group">
+                		<span>Status&nbsp;<span class="text-danger">*</span></span>
+                		<select class="form-control status">
+                			<option disabled selected>Please select status</option>
+                			<option value="1">Active</option>
+                			<option value="0">Inactive</option>
+                		</select>
+                	</div>
+                	<div class="cash-amount-warning"></div>
+                </div>
+                <div class="modal-footer">
+                	<button class="btn btn-primary btn-sm btn-save-change-status">Submit</button>
+                    <button class="btn btn-secondary btn-sm" data-dismiss="modal" aria-label="Close">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="message_modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Message</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary btn-sm" data-dismiss="modal" aria-label="Close">Okay</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 	<script type="text/javascript" src="<?= base_url();?>assets/js/user/customer.js"></script>
 <?php endif ?>
--- assets/js/user/customer.js
+++ assets/js/user/customer.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var userTable;
+	var selected_user_id = null;
 
 	get_customer();
 	function get_customer(){
@@ -49,7 +50,22 @@ $(document).ready(function(){
 				            	"data":"",
 				                "render": function(data, type, full, meta) {
 				                    var html = "";
-				                    html = '<a href='+base_url+"customer-view/"+full.encrypted_id+' class="btn btn-outline-success btn-sm"><i class="fa-solid fa-eye"></i></a>'
+				                    if(full.is_active == "1"){
+				                    	html = '<center><span class="status-active">Active</span></center>';
+				                    }
+				                    else{
+				                    	html = '<center><span class="status-inactive">Inactive</span></center>';
+				                    }
+				                    
+				                    return html;
+				                }
+				            },
+				            {
+				            	"data":"",
+				                "render": function(data, type, full, meta) {
+				                    var html = "";
+				                    html += '<div class="btn-group"><a href='+base_url+"customer-view/"+full.encrypted_id+' class="btn btn-outline-success btn-sm"><i class="fa-solid fa-eye"></i></a>'
+				                     html += '&nbsp;<button class="btn btn-outline-success btn-sm btn-change-status" title="Change Status"><i class="fa-solid fa-signal"></i></button></div>';
 				                    return html;
 				                }
 				            },
@@ -66,4 +82,61 @@ $(document).ready(function(){
 			}
 		})
 	}
+
+	$(document).on("click", ".btn-change-status", function(){
+		$("#change_status_modal").modal("show")
+		var data = userTable.row($(this).parents('tr')).data();
+
+		selected_user_id = data.id;
+		$(".status").val(data.is_active).trigger("change");
+	})
+
+	var loading_save_change_status = false;
+	$(".btn-save-change-status").on("click", function(){
+		if(!loading_save_change_status){
+			loading_save_change_status = true;
+
+			$(".btn-save-change-status").prop("disabled", true);
+
+			$(".global-loading").css({
+                "display": "flex"
+            })
+            createProcessLoading('.global-loading', '<span style="color:white;">Loading...</span>', base_url + 'assets/uploads/preloader/preloader_logo.gif', '80px', '80px', '24px')
+
+            $.ajax({
+            	url: base_url + "user/changeUserStatus",
+            	type: 'POST',
+            	dataType: 'json',
+            	data:{
+            		status: $(".status").val(),
+            		user_id: selected_user_id
+            	},
+            	success: function(response){
+            		if(response.is_error){
+            			$(".btn-save-change-status").prop("disabled", false);
+            			loading_save_change_status = false;
+            			$(".global-loading").css({
+	                        "display": "none"
+	                    })
+	                    $(".modal").modal("hide")
+	                    $("#message_modal").modal("show")
+	                    $("#message_modal .modal-body").html('<div class="text-danger">'+response.error_msg+'</div>');
+            		}
+            		else{
+            			window.location.reload()
+            		}
+            	},
+            	error: function(error){
+            		$(".btn-save-change-status").prop("disabled", false);
+        			loading_save_change_status = false;
+        			$(".global-loading").css({
+                        "display": "none"
+                    })
+                    $(".modal").modal("hide")
+                    $("#message_modal").modal("show")
+                    $("#message_modal .modal-body").html('<div class="text-danger">Unable to change status, please try again.</div>');
+            	}
+            })
+		}
+	})
 })
