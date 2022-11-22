008dbd4374246c19726f2e814f399f80f499313b
Derick Eugenio <eugenioderick@gmail.com>
2022-11-22 20:52:50 +0800

    about us page

--- application/views/company/about-us.php
+++ application/views/company/about-us.php
@@ -2,6 +2,35 @@
 	<div class="container-body" style="padding-top: 10px;">
 		<div class="about-us-content">
 			<p style="font-size: 22px">About Us</p>
+			<div class="image-container">
+				<img src="<?= base_url();?>assets/uploads/images/company-image.jpg">
+			</div>
+
+			<div class="description-container" style="margin-top: 40px;">
+				<p><b>Café De San Vicente</b> that serves simple, natural food. Accompanied by authentic recipes and brews. Most coffee shops in the Philippines don’t do much with food and most restaurants don’t worry about coffee. The <b>Café De San Vicente</b> is a hybrid combining a strong coffee culture and a robust food culture under one roof in a welcoming, cozy and comfortable.</p>
+				<p><b>Our Coffee</b> – We serve a range of espresso coffees made from the finest coffee beans. With their superior technical knowledge and a passion for making amazing coffee they will make you the perfect cuppa.</p>
+				<p><b>Our Food</b> – Our food is all natural, simple food made with local ingredients, wherever possible, to provide you with fresher, healthier and deliciously tasty meals. We have a well stocked pastry cabinet, our food has an international flavor.
+				It’s the perfect place to relax on your own or to catch up with friends while enjoying great coffee and great food.</p>
+				<p>We hope <b>Café De San Vicente</b> will become part of your lifestyle.</p>
+			</div>
+
+			<div class="address-container" style="margin-top: 60px;">
+				<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=900 San Marcelino St, Ermita, Manila, 1000 Metro Manila&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://piratebay-proxys.com/">Piratebay</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
+			</div>
+
+			<div class="business-hours" style="margin-top: 60px;">
+				<span>Regular Business Hours:</span><br>
+				<span>Mon-Fri: 9:00 AM-5:00PM</span><br>
+				<span>Sat-Sun: Closed</span>
+			</div>
+
+			<div class="contacts" style="margin-top: 40px;">
+				<span>If you have concerns please contact:</span><br>
+				<span>Jequerie.acido@adamson.edu.ph</span><br>
+				<span>Frances.nicole.casihan@adamson.edu.ph</span><br>
+				<span>Jaimelyn.boldadora@adamson.edu.ph</span><br>
+				<span>Gabrielle.falogme@adamson.edu.ph</span><br>
+			</div>
 		</div>
 	</div>
 </div>
--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -25,7 +25,7 @@
 			<div class="row">
 				<div class="col-12 col-sm-10"><strong>COMPANY</strong></div>
 			</div>
-			<a href="<?= base_url(); ?>about-us" class="about">About Us</a><br/>
+			<a href="<?= base_url(); ?>company/about-us" class="about">About Us</a><br/>
 			<a href="<?= base_url(); ?>company/terms-and-condition">Terms and Condition</a>
 		</div>
 		<div class="col-sm-4">
