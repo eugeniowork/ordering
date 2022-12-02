43327a921e79eeda0ab2168be7d3e9fde92b4803
Derick Eugenio <eugenioderick@gmail.com>
2022-12-02 13:21:31 +0800

    cash in receipt

--- application/helpers/receipt_helper.php
+++ application/helpers/receipt_helper.php
@@ -112,48 +112,87 @@
 		$user_in_charge_id = $cash_in_details['user_in_charge'];
 		$user_in_charge_details = $CI->global_model->get("users", "*", "id = '$user_in_charge_id'", [], "single", []);
 
+		$title = "ADAMSON UNIVERSITY <br> ADAMSON-OZANAM EDUCATIONAL INSTITUITIONS, INC.";
+		$company_address = "900 SAN MARCELINO ST. BRGY. 660-A ZONE 71 ERMITA MANILA";
+		$company_tin = "001356269-000";
+
 		$html = '
 			<style>
+				body{
+					color: black;
+					borders: 1px solid black;
+				}
 			    @page {
-			        margin: 0cm 0cm;
+			        margin: 0px 270px;
 			    }
 			</style>
-			<div class="logo-container" style="font-size: 12px;">
-				<center><img src="'.base_url().LOGO.'" style="width: 70px;height: 70px;"></center>
+			<br>
+			<div style="text-align:center; font-size: 10px; padding: 0px 10px;">
+				<span>'.$title.'</span><br>
+				<span>'.$company_address.'</span><br>
+				<span>VAT REGISTERED TIN# '.$company_tin.' NVAT</span><br>
 			</div><br>
 
-			<center style="color: #007bff">Cash In Receipt</center><br>
-
-			<div class="order-details-container" style="font-size: 12px; padding: 0px 20px;">
-				<span>Staff: <strong>'.$user_in_charge_details['firstname']." ".$user_in_charge_details['lastname'].'</strong></span><br>
-				<span>Date: <strong>'.date('M d, Y h:i a', strtotime($cash_in_details['created_date'])).'</strong></span><br>
-				<span>Reference No: <strong>'.$cash_in_details['reference_no'].'</strong></span><br>
-				<span>Amount: <strong>Php '.number_format($cash_in_details['request_amount'], 2).'</strong></span><br>
-				<span>Cash Amount: <strong>Php '.number_format($cash_in_details['cash_amount'], 2).'</strong></span><br>
-				<span>Change: <strong>Php '.number_format($cash_in_details['cash_amount'] - $cash_in_details['request_amount'], 2).'</strong></span><br>
+			<div style="text-align:center; font-size: 15px; padding: 0px 1px;">
+				<span>'.APPNAME.'</span>
 			</div><br>
+
+			<div style="font-size: 13px; padding: 0px 0px;">
+				<span>Date: '.date('d M Y', strtotime($cash_in_details['created_date'])).'</span>
+			</div><br>
+
+			<div style="font-size: 13px; padding: 0px 0px;">
+				<span>Receipt No.: '.$cash_in_details['reference_no'].'</span><br>
+				<span>Cashier Name: '.$user_in_charge_details['firstname']." ".$user_in_charge_details['lastname'].'</span><br>
+			</div><br>
+
+			<div style="font-size: 13px; padding: 0px 0px;">
+				<table width="100%" cellspacing="0" cellpadding="3">
+					<tbody>
+						<tr>
+							<td>Amount</td>
+							<td style="text-align: right;"><b>P'.number_format($cash_in_details['request_amount'],2).'</b></td>
+						</tr>
+						<tr>
+							<td>Cash Amount</td>
+							<td style="text-align: right;"><b>P'.number_format($cash_in_details['cash_amount'],2).'</b></td>
+						</tr>
+						<tr>
+							<td>Change</td>
+							<td style="text-align: right;"><b>P'.number_format($cash_in_details['cash_amount'] - $cash_in_details['request_amount'],2).'</b></td>
+						</tr>
+					</tbody>
+				</table>
+			</div><br><br><br>
+
+			<div style="text-align:center; font-size: 10px; padding: 0px 10px;">
+				<span>DATE ISSUED: '.date('Y-m-d', strtotime($cash_in_details['created_date'])).'</span><br>
+				<span>THIS SERVES AS AN OFFICIAL RECEIPT</span>
+				<span>"THIS DOCUMENT IS NOT VALID FOR CLAIM OF INPUT TAX"</span>
+			</div>
 		';
 
 		$CI->load->library('dompdf_library');
-		$options = new Dompdf\Options();
-	    $options->setTempDir('temp'); // temp folder with write permission
-	    $options->setIsRemoteEnabled(true);
 
-	    $dompdf = new Dompdf\DOMPDF();
-	    $dompdf->setOptions($options);
-	    $dompdf->set_option('enable_html5_parser', TRUE);
+		$options = new Dompdf\Options();
+		$options->setTempDir('temp'); // temp folder with write permission
+		$options->setIsRemoteEnabled(true);
+
+		$dompdf = new Dompdf\DOMPDF();
+		$dompdf->setOptions($options);
+		$dompdf->set_option('enable_html5_parser', TRUE);
 
 		$dompdf->loadHtml($html);
 
 		//DOM PDF SIZE CONVERTION
-	    //1 inch = 72 point
-	    //1 inch = 2.54 cm
-	    // 1 1/4 = 1.25 inch = 3.175 cm = 3.175/2.54*72 = 90 height
-	    //1 3/4 = 1.75 inch = 4.445 cm = 4.445/2.54*72 = 126 width
-	    // array('','',width, height)
-	    $dompdf->setPaper(array(0,0,200,300));
+		//1 inch = 72 point
+		//1 inch = 2.54 cm
+		// 1 1/4 = 1.25 inch = 3.175 cm = 3.175/2.54*72 = 90 height
+		//1 3/4 = 1.75 inch = 4.445 cm = 4.445/2.54*72 = 126 width
+		// array('','',width, height)
+		$dompdf->setPaper('A4', 'portrait');
 
-	    $dompdf->render();
+		$dompdf->render();
 
 	    $output = $dompdf->output();
 
